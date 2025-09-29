f15e602744f8b4e20b1224bafe83be9fdf3076d6
1080heidi <heidideleon415@gmail.com>
2025-09-28 20:08:15 -0700

    add movie.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
 // COMSC-210 | Lab 14 | Heidi Pico
 // IDE: VS CODE
 
+#include "Movie.h"
 #include <iostream>
-#include <string>
 using namespace std;
-
-    void setTitle(string t) { title = t;}
-    void setYear(int y) { year = y; } 
-    void setWriter(string w) { writer = w;}
-    
-    string getTitle() { return title; }
-    int getYear() { return year; }
-    string getWriter() { return writer; }
-
-    void print() {
-        cout << "Movie: " << title << endl;
-        cout << "    Year released: " << year << endl;
-        cout << "    Screenwriter: " << writer << endl;
-        cout << endl;
-    }
 
 int main() {
     Movie m("Test Movie", 2025, "Heidi Pico");
