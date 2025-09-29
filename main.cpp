ac6ecbfffcea8ce0b46db8f12081812f0950053a
1080heidi <heidideleon415@gmail.com>
2025-09-28 20:17:22 -0700

    read movies from input file into vector

--- main.cpp
+++ main.cpp
@@ -3,10 +3,32 @@
 
 #include "Movie.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 int main() {
-    Movie m("Test Movie", 2025, "Heidi Pico");
-    m.print();
-    return 0;
+    vecotr<Movie> movies;
+    ifstream fin("input.txt");
+
+    string t, w;
+    int y;
+
+    if (fin.good()){
+        while (getline(fin, t)) {
+            fin >> y;
+            fin.ignore();
+            getline(fin, w);
+
+            Movie temp(t, y, w);
+            movies.push_back(temp)
+        }
+        fin.close();
+    } else {
+        cout << "Input file not found." << endl;
+    }
+    
+    for (auto m : movies) {
+        m.print();
+    }
 }
