92e015f057a33322952535a1385588ee23241aa7
1080heidi <heidideleon415@gmail.com>
2025-09-28 19:37:04 -0700

    starting movie class

--- main.cpp
+++ main.cpp
@@ -1,2 +1,32 @@
 // COMSC-210 | Lab 14 | Heidi Pico
 // IDE: VS CODE
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// class here
+class Movie {
+private:
+    string title;
+    int year;
+    string writer;
+
+public: 
+    Movie() {
+        title = "";
+        year = 0;
+        writer = "";
+    }
+
+    Movie(string t, int y, string w) {
+        title = t;
+        writer = w;
+        year = y;
+    }
+
+    void setTitle(string t) { title = t;}
+    void setYear(int y) { year = y; } 
+    void setWriter(string w) { writer = w;}
+    
+    string getTitle() { return title; }
